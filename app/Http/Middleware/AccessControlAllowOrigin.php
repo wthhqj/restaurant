1bbb012b6c49a41c8b3a88fcf8140a244be791c7
ZhangHao <zhanghao2mail@gmail.com>
2017-12-20 16:31:12 +0800

    跨域支持options选项以及content-type

--- app/Http/Middleware/AccessControlAllowOrigin.php
+++ app/Http/Middleware/AccessControlAllowOrigin.php
@@ -16,6 +16,10 @@ class AccessControlAllowOrigin
     public function handle($request, Closure $next)
     {
         header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Credentials: true");
+        header("Content-Type: application/json; charset=utf-8");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        header("Access-Control-Allow-Headers: DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type");
         return $next($request);
     }
 }
